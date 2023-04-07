4780b2b9e2b2379078fb9c5af0877bc42332402b
amarjeet23 <amarjeetkohli17@gmail.com>
2023-04-07 07:57:29 -0700

    unit test added for services having no dependency

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -1,23 +1,27 @@
 import { MessageService } from "./message.service";
 describe("messageService", () => {
-  let messageService = new MessageService();
-  beforeEach(() => {
-    messageService = new MessageService();
-  });
+  let messageService:MessageService;
+  //   beforeEach(() => {
+  //     messageService = new MessageService();
+  //   });
 
   //   1.
   it("should have no element when start", () => {
+    messageService = new MessageService();
     expect(messageService.messages.length).toBe(0);
   });
 
   //   2.
   it("should add an element when add is clicked", () => {
+    messageService = new MessageService();
+
     messageService.add("message1");
     expect(messageService.messages.length).toBe(1);
   });
 
   //   3.
   it("it should have no item when clear is called", () => {
+    messageService = new MessageService();
     // let newItem = "test3";
     messageService.add("message2");
     messageService.clear();
